6a7fd1b73d4cb702a7dd82344d0ae8ab14bddeb1
Duong Van Nhat <nhatn42003bn@gmail.com>
2024-08-31 19:12:52 +0700

    Kết Nối Tới MySQL Database với Sequelize - Render Dữ Liệu Ra View

---
+++ src/route/web.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import homeControllers from '../controllers/homeControllers';
+
+let router = express.Router();
+
+let initWebRoutes = (app) => {
+    router.get('/', homeControllers.getHomePage);
+
+    return app.use('/', router);
+};
+
+module.exports = initWebRoutes;
